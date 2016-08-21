41e776d1c9be7df6986c78ea961386dc63076c7d
MartyLake <matthieu@talbot.audio>
2016-08-21 20:50:50 +0200

    Removed HelloCute project

--- Tasklist.h
+++ Tasklist.h
@@ -1,7 +1,7 @@
 /*Task List
 =========
 
-//TODO compile cute project
+//TODO compile cute project -> install boost ?
 //TODO add lua hpp and lua sources
 //TODO add minimal test provine lua is available in the project
 //TODO convert the project to cmake
