0d5f7ea1af95b66f37a8b2adced8f07f25ae5b70
MartyLake <matthieu@talbot.audio>
2016-08-21 15:08:17 +0200

    Add tasklist.h that contains general TODOs for the project.

---
+++ HelloCute/TaskList.h
@@ -0,0 +1,10 @@
+/*Task List
+=========
+
+//TODO create reamde.txt
+//TODO compile cute project
+//TODO add lua hpp and lua sources
+//TODO add minimal test provine lua is available in the project
+//TODO convert the project to cmake
+//TODO add qt dependencies
+*/
