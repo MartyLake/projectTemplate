91efc3a1b188e1db4a3baebe68fe5171aa4012f6
MartyLake <matthieu@talbot.audio>
2016-09-25 17:40:18 +0200

    Reset. Use CMAKE from the beginning.

--- Tasklist.h
+++ Tasklist.h
@@ -1,9 +1,20 @@
 /*Task List
 =========
 
+//TODO create cmake cpp project hello world https://cmake.org/cmake-tutorial/
+//TODO add boost test : http://www.boost.org/doc/libs/1_53_0/libs/test/doc/html/utf/tutorials.html
 //TODO compile cute project -> install boost ?
 //TODO add lua hpp and lua sources
 //TODO add minimal test provine lua is available in the project
 //TODO convert the project to cmake
 //TODO add qt dependencies
+*/
+
+/*
+How to compile for windows:
+- Install ninja (download at https://github.com/ninja-build/ninja/releases v1.7.1, and copy to path)
+- in the project folder: mkdir ../projectTemplateBuild
+- cd ../projectTemplateBuild
+- cmake -G "Eclipse CDT4 - Ninja" ../projectTemplate
+- eclipse -> open project from file system ????
 */
